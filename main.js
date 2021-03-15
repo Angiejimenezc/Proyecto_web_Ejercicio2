75e9faf1d27a1d51da12b8fedd5e0124a92e10ee
Angie Jimenez <angiejimenez@gmail.com>
2021-03-15 09:39:19 +0100

    git commit -m

---
+++ main.js
@@ -0,0 +1,8 @@
+ 
+// Añado Evento para mostrar botón Menu al hacer Click
+document.querySelector(".menu-btn").addEventListener("click", () => {
+//  Clase Show del Elemento nav-menu (Para mostrar o no mostrar el menú)
+    document.querySelector(".nav-menu").classList.toggle("mostrar");
+});
+
+
